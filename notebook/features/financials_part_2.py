81026e603e753f4dca14f504318c067677342fb9
Charlotte <charlotte.simon@hec.edu>
2023-03-11 22:48:14 +0100

    uppdate

---
+++ notebook/features/financials_part_2.py
@@ -0,0 +1,173 @@
+from shapely.geometry import Point
+import os
+import csv
+from pathlib import Path
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import re
+import geopandas as gpd
+from shapely.geometry import shape
+from shapely.geometry import Point, LineString, MultiPoint
+import datetime
+from tqdm import tqdm
+import math
+
+# Hydrogen fuel price
+# source: https://www.autonews.fr/green/voiture-hydrogene#:~:text=Quant%20au%20plein%20d%27hydrog%C3%A8ne,diminuer%20avec%20son%20d%C3%A9veloppement%20progressif.
+# source: https://reglobal.co/cost-of-ownership-of-fuel-cell-hydrogen-trucks-in-europe/
+
+# H2 price can vary between 10 and 15 euros depending on the source. Assuming that in our scenario we have no competitors, an assumption we will relax in part 3, we can
+# set the price of H2 to 15e
+H2_price_2023 = 10
+H2_price_2030 = 7 
+H2_price_2040 = 4 
+
+# Refueling time
+# source : https://www.pcmag.com/news/volvos-hydrogen-powered-truck-can-travel-620-miles-before-needing-to-refuel
+refueling = 10 #min
+
+
+station_info = {
+    'station_type': ['small', 'medium', 'large'],
+    'capex': [3, 5, 8],
+    'depreciation': [0.15,0.15,0.15],
+    'opex': [0.1 * 3, 0.08 * 3, 0.07 * 3],
+    'storage': [2, 3, 4],
+    'construction_time': [1, 1, 1],
+    'footprint': [650, 900, 1200],
+    'prof_threshold': [0.9, 0.8, 0.6]
+}
+
+df_station_info = pd.DataFrame(station_info).reset_index(drop=True)
+total_demand = 122100
+
+def threshold(df_station_info :pd.DataFrame)-> float:
+    '''
+    This functions calculates the necessary amount of sales in T/day to be profitable depending on station's sizes
+    '''
+    df_station_info['threshold'] = df_station_info['prof_threshold']* df_station_info['storage']
+    return df_station_info
+
+# Calculate Demand
+
+def station_size(df_station: pd.DataFrame,df_stations_info)-> pd.DataFrame:
+    '''
+    This function derives the station size depending on quantity sold and the profitability thresholds
+    '''
+    df_station['Quantity_sold_per_year(in kg)']= df_station['Quantity_sold_per_day(in kg)']*365
+
+
+def preprocess_station(df_station: pd.DataFrame,df_tmja: pd.DataFrame) -> pd.DataFrame:
+    '''
+    This functions adds traffic information relative to the stations
+    '''
+    df_station = df_station.drop(['URL','Lavage','Paiement'],axis =1)
+    df_tmja_group = df_tmja.groupby('route').agg({'percentage_traffic':'sum','TMJA_PL':'sum'})
+    df_tmja_group['route'] = df_tmja_group.index
+    df_tmja_group = df_tmja_group.reset_index(drop=True)
+    df_station['route_id'] = df_tmja.iloc[df_station['Closer_route_by_index']]['route'].iloc[0]
+    df_station_1 = pd.merge(df_station, df_tmja_group, on='route',how ='left')
+    
+    return df_station
+
+def sales(df_station:pd.DataFrame,year: float)-> pd.DataFrame:
+    '''
+    This functions calculates the amount sold at each new stations in kg/day.
+    '''
+    h2_price_dict = {2023: 10, 2030: 7, 2040: 4}
+    if year not in h2_price_dict:
+        raise ValueError('Year can only be 2023, 2030 or 2040')
+    
+    df_station['Quantity_sold_per_day(in kg)'] = total_demand * df_station['percentage_traffic'] *  (1 / df_station.groupby('route')['route'].transform('count'))
+    df_station['Revenues_day'] = df_station['Quantity_sold_per_day(in kg)']  * h2_price_dict[year]
+
+    return df_station
+
+def station_type(df_station:pd.DataFrame, df_station_info: pd.DataFrame)-> pd.DataFrame:
+    '''
+    This function derives the station size depending on quantity sold and the profitability thresholds
+    '''
+    info = threshold(df_station_info)
+    small_prof_threshold, medium_prof_threshold, large_prof_threshold = info['threshold']
+    df_station['Quantity_sold_per_year(in kg)']= df_station['Quantity_sold_per_day(in kg)']*365
+
+    df_station['not_prof'] = (df_station['Quantity_sold_per_year(in kg)']/1000< small_prof_threshold).astype(int)
+
+    df_station['small_station'] = ((df_station['Quantity_sold_per_year(in kg)']/1000>= small_prof_threshold) &
+                                   (df_station['Quantity_sold_per_year(in kg)']/1000< medium_prof_threshold)).astype(int)
+    
+    df_station['medium_station'] = ((df_station['Quantity_sold_per_year(in kg)']/1000>= medium_prof_threshold) &
+                                    (df_station['Quantity_sold_per_year(in kg)']/1000< large_prof_threshold)).astype(int)
+    
+    df_station['large_station'] = (df_station['Quantity_sold_per_year(in kg)']/1000>= large_prof_threshold).astype(int)
+    
+    df_station['station_type'] = df_station.apply(lambda row: 
+    'not profitable' if row['not_prof'] == 1 
+    else 'small' if row['small_station'] == 1 
+    else 'medium' if row['medium_station'] == 1 
+    else 'large' if row['large_station'] == 1 
+    else 'unknown', axis=1)
+    return df_station
+
+def financials(df_station:pd.DataFrame, df_station_info: pd.DataFrame,year: float)-> pd.DataFrame:
+    '''
+    This function provides an overview of the P&L for each station
+    '''
+    df_station = station_type(df_station, df_station_info)
+    df_station = sales(df_station,year)
+    df_station['Revenues'] = df_station['Revenues_day'] * 365
+    df_station['EBITDA'] = df_station['Revenues']- df_station['station_type'].map(df_station_info.set_index('station_type')['opex'])
+    df_station['Opex'] = df_station['Revenues'] - df_station['EBITDA']
+
+    df_station_info['yearly_depreciation'] = df_station_info['capex'] * df_station_info['depreciation']
+    df_station['EBIT'] = df_station['Revenues']- df_station['station_type'].map(df_station_info.set_index('station_type')['opex'])
+    df_station['depreciation'] = df_station['EBITDA'] - df_station['EBIT']
+
+    fin = ['Revenues','EBITDA','EBIT','depreciation','Opex']
+    df_station[fin] = df_station[fin].fillna(0)
+    return df_station
+
+def capex(df_station:pd.DataFrame, df_station_info: pd.DataFrame)-> pd.DataFrame:
+    '''
+    This function provides an overview of the capex needs for each station
+    '''
+    df_station['CAPEX'] = df_station['Revenues']- df_station['station_type'].map(df_station_info.set_index('station_type')['capex'])
+    return df_station
+
+def financial_summary_1(df_station:pd.DataFrame) -> pd.DataFrame:
+    '''
+    This functions give the consolidated financials of the deployment of all the stations
+    '''
+    
+    df_station = financials(df_station, df_station_info)
+    df_station = capex(df_station, df_station_info)
+
+    total_revenues, total_EBITDA = df_station['Revenues'].sum(), df_station['EBITDA'].sum()
+    total_opex = total_revenues - total_EBITDA
+    total_EBIT = df_station['EBIT'].sum()
+    total_capex = df_station['CAPEX'].sum()
+    summary_df = pd.DataFrame({
+        'Total Capex': [total_capex],
+        'Total Revenues': [total_revenues],
+        'Total Opex': [total_opex],
+        'Total EBITDA': [total_EBITDA],
+        'Total EBIT': [total_EBIT]
+    })
+    return summary_df
+
+def financial_summary(df_station:pd.DataFrame,df_station_info: pd.DataFrame,year: float) -> pd.DataFrame:
+    '''
+    This function gives the consolidated financials of the deployment of all the stations
+    '''
+    
+    df_station = financials(df_station, df_station_info,2030)
+    df_station = capex(df_station, df_station_info)
+
+    summary_df = pd.pivot_table(df_station, values=['CAPEX','Revenues','Opex', 'EBITDA', 'depreciation', 'EBIT'], 
+                            index=[],
+                            columns=['station_type'], 
+                            aggfunc=np.sum, fill_value=0)
+    summary_df.loc['Total'] = summary_df.sum()
+    
+    return summary_df
---
+++ notebook/features/question_2.py
@@ -0,0 +1,155 @@
+import pandas as pd
+import numpy as np
+from shapely.geometry import Point
+import geopandas as gpd
+from tqdm import tqdm 
+from pyproj import Proj, transform
+
+from preprocessing.helping_functions import *
+from preprocessing.pre_process_stations import *
+from preprocessing.pre_process_traffic import *
+
+from features.financials_part_2 import *
+from features.config import *
+
+
+def indicate_crs(shp_file: gpd.geodataframe.GeoDataFrame, epsg:str):
+    """Sets the coordinate reference system (CRS) of a GeoDataFrame.
+
+    Parameters:
+    -----------
+    shp_file : gpd.geodataframe.GeoDataFrame
+        The GeoDataFrame to set the CRS for.
+    epsg : str
+        The EPSG code for the target CRS.
+
+    Returns:
+    --------
+    gpd.geodataframe.GeoDataFrame
+        The GeoDataFrame with the CRS set."""
+
+    shp_file.set_crs(epsg, allow_override=True)
+    return shp_file
+
+def fix_stations(df):
+    df['Coordinates'] = df['Coordinates'].apply(lambda x: x.replace(',,', ','))
+    return df
+
+
+def new_coordinates_creation(
+        approximate_nb_of_points, 
+        shapefile_tmja):
+    """Creates new coordinates along a route by splitting the route into equal
+    length segments and interpolating new points along each segment.
+​
+    Parameters:
+    -----------
+    approximate_nb_of_points : int
+        The approximate number of points to create along each route.
+    shapefile_tmja : gpd.geodataframe.GeoDataFrame
+        The shapefile to create new coordinates for.
+
+    Returns:
+    --------
+    gpd.geodataframe.GeoDataFrame
+        A GeoDataFrame containing the new coordinates.
+    """
+
+    total_distance = shapefile_tmja.geometry.length.sum() # in meters
+    distance_between_coordinates = total_distance/approximate_nb_of_points
+    points = []
+    routes = []
+
+    for idx in tqdm(shapefile_tmja.index):
+        line = shapefile_tmja.geometry[idx]
+        route = shapefile_tmja.route[idx]
+        n_splits = int(line.length/distance_between_coordinates)
+        
+        if n_splits < 2:
+            splitter = [line.interpolate((i/2), normalized=True) for i in range(2)]
+        else:
+            splitter = [line.interpolate((i/n_splits), normalized=True) for i in range(n_splits)]
+
+        points.append(splitter)
+        routes.append([route for i in (range(n_splits) if n_splits >= 2 else range(2))])
+        
+    routes = np.concatenate(routes)
+    
+    coordinates = pd.DataFrame([i.wkt.replace('POINT (', '').replace(')', '').split(' ') for i in np.concatenate(points)])
+    coordinates.columns = ["easting", "northing"]
+    coordinates['route'] = routes
+    coordinates['geometry'] = gpd.points_from_xy(x=coordinates.easting, 
+                                                 y=coordinates.northing,
+                                                 crs=shapefile_tmja.crs)
+    
+    shp_coordinates = gpd.GeoDataFrame(coordinates)
+    
+    return shp_coordinates
+
+def station_distances_all(
+        df_stations: pd.DataFrame
+):
+    '''
+    Add columns of the distances between each stations 
+    '''
+
+    for i in tqdm(df_stations['index']):
+        station_index = df_stations.loc[i, 'index']
+        df_stations[f'distance_to_point_{station_index}'] = df_stations.loc[i, 'geometry'].distance(df_stations.geometry)
+
+    return df_stations
+
+def get_closer_station(
+        df_stations_complete: pd.DataFrame
+):
+    '''
+    Create a columns distance_closer_station with the smaller distance between each stations
+    '''
+    
+    columns_distance = [x for x in df_stations_complete.columns if x.startswith('distance_to_point_')==True]
+    df_stations_complete['distance_closer_station'] = df_stations_complete[columns_distance].apply(lambda x: min(x[x!=0]), axis=1)
+
+    return df_stations_complete
+
+def distance_to_hub(
+        hub_denses: pd.DataFrame,
+        hub_elargies: pd.DataFrame,
+        stations: pd.DataFrame):
+    
+    '''
+    Add 4 columns regarding the distances to the closest hubs, for each point:
+    - closest_dense_hub: the index of the closest dense hub 
+    - closest_elargie_hub: the index of the closest enlarged hub 
+    - distance_to_closest_dense_hub: the distance to the closest dense hub
+    - distance_to_closest_elargie_hub: the distance to the closest enlarged hub 
+    '''
+
+    dense_hub_list_denses = []
+    dense_hub_list_elargies = []
+    distance_to_dense_hub_list_denses = []
+    distance_to_dense_hub_list_elargies = []
+
+    for idx_station in tqdm(stations.index):
+        # for dense hubs
+        geodf_denses = pd.DataFrame(hub_denses['geometry'].distance(stations.loc[idx_station,'geometry']))
+        min_distance_denses = geodf_denses.min()
+        idx_hub = geodf_denses.min().index[0]
+        dense_hub_list_denses.append(hub_denses.at[idx_hub, 'e1'])
+        distance_to_dense_hub_list_denses.append(min_distance_denses)
+
+        # for enlarged hubs 
+        geodf_elargies = pd.DataFrame(hub_elargies['geometry'].distance(stations.loc[idx_station,'geometry']))
+        min_distance_elargies = geodf_elargies.min()
+        idx_hub = geodf_elargies.min().index[0]
+        dense_hub_list_elargies.append(hub_elargies.at[idx_hub, 'e1'])
+        distance_to_dense_hub_list_elargies.append(min_distance_elargies)
+
+    stations['closest_dense_hub'] = dense_hub_list_denses
+    stations['distance_to_closest_dense_hub'] = distance_to_dense_hub_list_denses
+
+    stations['closest_elargie_hub'] = dense_hub_list_elargies
+    stations['distance_to_closest_large_hub'] = distance_to_dense_hub_list_elargies
+
+    stations = stations.reset_index(drop=False)
+
+    return stations
