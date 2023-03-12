d1b77add7bbd203206cf69a6c6a4925f27c5781a
Augustin de la Brosse <augustin2164@gmail.com>
2023-03-12 12:56:22 +0100

    created plotting installation function

--- helping_functions.py
+++ helping_functions.py
@@ -298,3 +298,67 @@ def compute_distance_to_each_station(geodf):
         geodf[f'distance_to_{URL}'] = geodf.loc[i, 'geometry'].distance(geodf.geometry)
     return geodf
 
+def plot_results(roads_shapefile:gpd.geodataframe.GeoDataFrame, df_results:pd.core.frame.DataFrame):
+    """
+    Plot the results of a simulation of H2 station placement on a map.
+
+    Parameters:
+        roads_shapefile : GeoDataFrame
+            A GeoDataFrame containing the roads' shapefile data.
+
+        df_results : DataFrame
+            A DataFrame containing the H2 station placement simulation results.
+            It should contain a 'geometry' column containing the stations' location in
+            string or Shapely.geometry format, and a 'station_type' column with the
+            type of station (i.e., small, medium, large).
+
+    Returns:
+        A plot of the roads and H2 stations overlaid on a map.
+
+    """
+    roads = gpd.GeoDataFrame({'geometry': roads_shapefile.geometry, 
+                              'type': ['route' for i in range(len(roads_shapefile.geometry))],
+                              'nom': roads_shapefile.route}, 
+                             crs=roads_shapefile.crs)
+    
+    if type(df_results.geometry[:1].values[0]) == str:
+        df_results = convert_str_geometry_to_geometry_geometry(results)
+        
+    stations = gpd.GeoDataFrame({'geometry': df_results.geometry, 
+                                 'type': [f'{i} H2 station' for i in df_results["station_type"]],
+                                 'nom': [f'H2 station n{i}' for i in range(df_results.shape[0])]}, 
+                                crs=roads_shapefile.crs)
+    
+    shp_file = pd.concat([roads, stations])
+    exploration = shp_file.explore(column='type', cmap='tab10')
+    
+    return exploration
+    
+def plotting_installations(df:pd.core.frame.DataFrame):
+    """
+    Plot the installation dates of H2 stations on a map.
+
+    Parameters:
+        df : DataFrame
+            A DataFrame containing data on H2 stations' installation, including their
+            location and installation date.
+
+    Returns:
+        A plot of the H2 station locations overlaid on a map, with the color of each
+            station indicating its installation date.
+
+    """
+
+    if type(df.geometry[:1].values[0]) == str:
+        df = convert_str_geometry_to_geometry_geometry(df)
+        
+    shp_file = gpd.GeoDataFrame(df, crs="epsg:2154")
+    shp_file = shp_file[['URL', 'nom_region', 'geometry', 'closest_road',
+       'closest_large_hub', 'closest_dense_hub', 'TMJA_PL', 'percentage_traffic',
+       'Quantity_sold_per_day(in kg)', 'Revenues', 'bool', 'size',
+       'Quantity_sold_per_year(in kg)', 'station_type', 'Revenues_day',
+       'EBITDA', 'Opex', 'EBIT', 'depreciation', 'date_installation']]
+    
+    exploration = shp_file.explore(column="date_installation", cmap="Blues")
+    
+    return exploration
